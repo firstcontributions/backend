26be5dd58d058845de653deede537300fce99ac3
Gokul T P <tp.gokul@gmail.com>
2020-09-26 16:50:48 +0530

    oauth setup

--- internal/profile/models/mongo/db.go
+++ internal/profile/models/mongo/db.go
@@ -1,8 +1,16 @@
 package mongo
 
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 const (
 	// DBProfile saves profile related informations
 	DBProfile         = "profile"
 	CollectionProfile = "profile"
 	CollectionTokens  = "tokens"
 )
+
+func getCollection(conn *mongo.Client) {
+
+}
