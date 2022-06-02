cbe7a11fb44dae82c9196158c4b8753ba43f5a1e
Gokul TP <tp.gokul@gmail.com>
2022-06-02 23:29:22 +0100

    get issue by id

--- internal/models/issuesstore/githubstore/issuebyid.go
+++ internal/models/issuesstore/githubstore/issuebyid.go
@@ -6,6 +6,18 @@ import (
 	"github.com/firstcontributions/backend/internal/models/issuesstore"
 )
 
+type GetIssueByIDQuery struct {
+	Node struct {
+		Issue GitIssue `graphql:"... on Issue"`
+	} `graphql:"node(id:$id)"`
+}
+
 func (i *GitHubStore) GetIssueByID(ctx context.Context, id string) (*issuesstore.Issue, error) {
-	return nil, nil
+	query := &GetIssueByIDQuery{}
+	if err := i.Query(ctx, query, map[string]interface{}{
+		"id": id,
+	}); err != nil {
+		return nil, err
+	}
+	return issueFromGithubIssue(query.Node.Issue), nil
 }
