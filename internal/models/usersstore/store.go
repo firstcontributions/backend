ae8b1e398dfae459e77b592c558efe616183f382
Roshan Jossy <roshanjossey@gmail.com>
2022-10-23 15:06:56 +0200

    add repository updated at property for issue sortBy

--- internal/models/usersstore/store.go
+++ internal/models/usersstore/store.go
@@ -7,14 +7,6 @@ import (
 )
 
 type Store interface {
-	// badge methods
-	GetBadgeByID(ctx context.Context, id string) (*Badge, error)
-	GetOneBadge(ctx context.Context, filters *BadgeFilters) (*Badge, error)
-	GetBadges(ctx context.Context, filters *BadgeFilters, after *string, before *string, first *int64, last *int64, sortBy BadgeSortBy, sortOrder *string) ([]*Badge, bool, bool, []string, error)
-	CountBadges(ctx context.Context, filters *BadgeFilters) (int64, error)
-	CreateBadge(ctx context.Context, badge *Badge, ownership *authorizer.Scope) (*Badge, error)
-	UpdateBadge(ctx context.Context, id string, update *BadgeUpdate) error
-	DeleteBadgeByID(ctx context.Context, id string) error
 	// user methods
 	GetUserByID(ctx context.Context, id string) (*User, error)
 	GetOneUser(ctx context.Context, filters *UserFilters) (*User, error)
@@ -23,4 +15,12 @@ type Store interface {
 	CreateUser(ctx context.Context, user *User, ownership *authorizer.Scope) (*User, error)
 	UpdateUser(ctx context.Context, id string, update *UserUpdate) error
 	DeleteUserByID(ctx context.Context, id string) error
+	// badge methods
+	GetBadgeByID(ctx context.Context, id string) (*Badge, error)
+	GetOneBadge(ctx context.Context, filters *BadgeFilters) (*Badge, error)
+	GetBadges(ctx context.Context, filters *BadgeFilters, after *string, before *string, first *int64, last *int64, sortBy BadgeSortBy, sortOrder *string) ([]*Badge, bool, bool, []string, error)
+	CountBadges(ctx context.Context, filters *BadgeFilters) (int64, error)
+	CreateBadge(ctx context.Context, badge *Badge, ownership *authorizer.Scope) (*Badge, error)
+	UpdateBadge(ctx context.Context, id string, update *BadgeUpdate) error
+	DeleteBadgeByID(ctx context.Context, id string) error
 }
